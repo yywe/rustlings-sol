ea3ce35a82295dd78605ad40dec98b83101fdb1b
Yongyong Wei <stweiyy@gmail.com>
2022-10-01 21:11:50 -0400

    solutions for rustings

---
+++ exercises/clippy/target/debug/deps/clippy3-e3cd98bf86df3631.d
@@ -0,0 +1,7 @@
+/home/yy/learning/rust/rustlings/exercises/clippy/target/debug/deps/clippy3-e3cd98bf86df3631.rmeta: clippy3.rs
+
+/home/yy/learning/rust/rustlings/exercises/clippy/target/debug/deps/clippy3-e3cd98bf86df3631.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
